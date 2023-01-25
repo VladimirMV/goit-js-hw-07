1269e1d7820febb32b29d401a126bee441098a21
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-01-25 14:47:09 +0200

    7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+console.log(galleryContainer);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+         
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" /> </a>
+        `;
+    })
+    .join("");
+    };
+
+
+    new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        
+    });
+
+ 
